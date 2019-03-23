0fceda735d5cd4d5ff8512a0cfe15b88ddae0389
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-03-23 17:32:44 -0500

    createOrupdate

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -15,12 +15,28 @@ let MetricStub = {
 let AgentStub = null
 let sandbox = null
 let single = Object.assign({}, agentFixtures.single)
+let uuid = single.uuid
 let id = 1
+let uuidArgs = {
+  where: {
+    uuid
+  }
+}
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
   AgentStub = {
     hasMany: sandbox.spy()
   }
+  // Model update Stub
+AgentStub.update = sandbox.stub()
+AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+  // Model findOne Stub
+
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.findByUuid(uuid)))
+  // Model findbyId Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.findById(id)))
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
@@ -45,5 +61,16 @@ test.serial('Setup', t => {
 
 test.serial('Agent#findById', async t => {
   let agent = await db.Agent.findById(id)
+  t.true(AgentStub.findById.called, 'findById sholud be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById sholud be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById sholud be called with id')
   t.deepEqual(agent, agentFixtures.findById(id), 'sholud be the same')
+})
+
+test.serial('Agent#createOrUpdate - exist', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+  t.true(AgentStub.findOne.called, 'findOne sholud be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne sholud be called twice')
+  t.true(AgentStub.update.calledOnce, 'update sholud be called once')
+  t.deepEqual(agent, single, 'agent should be the same')
 })
