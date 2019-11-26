a68ce6fee851e2d7292ef5d5319ab06465947fd4
Alexis Holguin <wholguinmor@uniminuto.edu.co>
2019-11-26 15:22:22 -0500

    cambios

--- platziverse-deploy/roles/platziverse-web/files/platziverse-web/config.js
+++ platziverse-deploy/roles/platziverse-web/files/platziverse-web/config.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = {
-  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
-  serverHost: process.env.SERVER_HOST || 'http://localhost:8080',
+  endpoint: process.env.API_ENDPOINT || 'http://142.93.116.89',
+  serverHost: process.env.SERVER_HOST || 'http://142.93.1.199',
   mqttHost: process.env.MQTT_HOST || 'mqtt://localhost',
   apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTAyMzkzNDExfQ.XMKKy9sgqA0TDKjCcgA4_784H2wP7RVQocttSTE-RTU'
 }
